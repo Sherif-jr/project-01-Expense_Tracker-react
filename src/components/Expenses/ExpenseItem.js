dc728ffa9a3ce267f2649fcfb12d42e2790fcf85
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-01-27 14:07:07 +0200

    Filter enabled.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
+  // const [title, setTitle] = useState(props.title);
+  // console.log("ExpenseItem evaluated by React");
 
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
+
   // testing timeout function...
   // const changeOnTime = (time) => {
   //   setTimeout(() => {
@@ -23,10 +24,9 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
